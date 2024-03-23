389167b62261b1ec423429db65ea41e1119bec07
Youtpout <EddyBoughioul@hotmail.fr>
2024-03-23 18:41:01 +0100

    add middleware

--- app/frames/frames.ts
+++ app/frames/frames.ts
@@ -1,3 +1,28 @@
+import { farcasterHubContext, openframes } from "frames.js/middleware";
 import { createFrames } from "frames.js/next";
+import { getXmtpFrameMessage, isXmtpFrameActionPayload } from "frames.js/xmtp";
 
-export const frames = createFrames({ basePath: "/frames" });
+export const frames = createFrames({
+  basePath: "/frames", middleware: [
+    farcasterHubContext({
+      hubHttpUrl: process.env.DEFAULT_DEBUGGER_HUB_URL,
+    }),
+    openframes({
+      clientProtocol: {
+        id: "xmtp",
+        version: "2024-02-09",
+      },
+      handler: {
+        isValidPayload: (body: JSON) => isXmtpFrameActionPayload(body),
+        getFrameMessage: async (body: JSON) => {
+          if (!isXmtpFrameActionPayload(body)) {
+            return undefined;
+          }
+          const result = await getXmtpFrameMessage(body);
+
+          return { ...result };
+        },
+      },
+    }),
+  ],
+});
