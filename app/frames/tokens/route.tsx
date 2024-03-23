1703100659a42f18ac76b1d0e59a971bbda80062
Youtpout <EddyBoughioul@hotmail.fr>
2024-03-23 17:58:44 +0100

    update for vercel

--- app/frames/tokens/route.tsx
+++ app/frames/tokens/route.tsx
@@ -9,41 +9,62 @@ import { FarcasterUserERC20BalancesOutputData } from "@airstack/frames";
 const totalPages = 5;
 
 const handleRequest = frames(async (ctx) => {
-  const pageIndex = Number(ctx.searchParams.pageIndex || 0);
+  try {
+    let tokens: (FarcasterUserERC20BalancesOutputData | null)[] = [];
 
-  let tokens: (FarcasterUserERC20BalancesOutputData | null)[] = [];
+    var url = new URL(
+      "/api/token",
+      vercelURL() || "http://localhost:3000"
+    )
+    const callApi = await fetch(url);
+    tokens = await callApi.json();
+    console.log("tokens", tokens);
 
-  var url = new URL(
-    "/api/token",
-    vercelURL() || "http://localhost:3000"
-  )
-  const callApi = await fetch(url);
-  tokens = await callApi.json();
-  console.log("tokens", tokens);
-
-  const listItems = tokens.map((data) =>
-    <li key={data?.tokenAddress}>{data?.name}</li>
-  );
+    const listItems = tokens.map((data) =>
+      <li key={data?.tokenAddress}>{data?.name}</li>
+    );
 
 
-  return {
-    image: (
-      <div tw="flex flex-col">
-        <ul tw="flex flex-col">
-          {listItems}
-        </ul>
-      </div>
-    ),
-    buttons: [
-      <Button
-        action="post"
-        target="/"
-      >
-        ← back
-      </Button>
-    ],
-    textInput: "Action",
-  };
+    return {
+      image: (
+        <div tw="flex flex-col">
+          <ul tw="flex flex-col">
+            {listItems}
+          </ul>
+        </div>
+      ),
+      buttons: [
+        <Button
+          action="post"
+          target="/"
+        >
+          ← back
+        </Button>
+      ],
+      textInput: "Action",
+    };
+  } catch (error) {
+    console.log(error);
+
+    return {
+      image: (
+        <div tw="flex flex-col">
+          Didnt work
+        </div>
+      ),
+      buttons: [
+        <Button
+          action="post"
+          target="/"
+        >
+          ← back
+        </Button>
+      ],
+      textInput: "Action",
+    };
+  }
+
+
 });
 
 export const POST = handleRequest;
