17c51277a2267cb6740c08e2f46aacf931312f13
Youtpout <EddyBoughioul@hotmail.fr>
2024-03-24 17:46:13 +0100

    revert middleware

--- app/frames/frames.ts
+++ app/frames/frames.ts
@@ -2,28 +2,16 @@ import { farcasterHubContext, openframes } from "frames.js/middleware";
 import { createFrames } from "frames.js/next";
 import { getXmtpFrameMessage, isXmtpFrameActionPayload } from "frames.js/xmtp";
 
-const DEFAULT_DEBUGGER_URL =
-  process.env.DEBUGGER_URL ?? "http://localhost:3010/";
-
-export const DEFAULT_DEBUGGER_HUB_URL =
-  process.env.NODE_ENV === "development"
-    ? new URL("/hub", DEFAULT_DEBUGGER_URL).toString()
-    : undefined;
-
-
 export const frames = createFrames({
   basePath: "/frames",
-  initialState: {
-    pageIndex: 0,
-  },
   middleware: [
     farcasterHubContext({
-      hubHttpUrl: DEFAULT_DEBUGGER_HUB_URL,
+      hubHttpUrl: process.env.DEFAULT_DEBUGGER_HUB_URL,
     }),
     openframes({
       clientProtocol: {
         id: "xmtp",
-        version: "2024-02-09",
+        version: "2024-02-01",
       },
       handler: {
         isValidPayload: (body: JSON) => isXmtpFrameActionPayload(body),
